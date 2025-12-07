c5d43f7537727852c2dd545bb7ab11d3b529a7df
misinizana <zaanamisinii@gmail.com>
2025-12-07 13:22:27 +0100

    add netfily url to cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -138,6 +138,7 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",  
+    'https://unrivaled-starburst-8d4d54.netlify.app',
 ]
 
 REST_FRAMEWORK = {
